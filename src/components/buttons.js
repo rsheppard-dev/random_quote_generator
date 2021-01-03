8338e75b5ce83c865dc8da533be97ec49e4ff6e8
rsheppard83 <rsheppard83@gmail.com>
2021-01-03 10:22:01 +0000

    button fix

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,6 +1,6 @@
 const Buttons = ({handleclick, quote, author}) => (
     <div className="buttons pt-4 d-flex justify-content-around">
-        <button className="btn btn-dark mr-1" onClick={handleclick}><i className="fas fa-comment-dots"></i> New Quote</button>
+        <button className="btn btn-dark mr-1" onClick={handleclick}><i className="fas fa-comment-dots"></i> New</button>
         <a href={`http://www.twitter.com/intent/tweet?text=${quote} --${author}`} target="_blank" rel="noreferrer" className="btn btn-dark ml-1"><i className="fab fa-twitter-square"></i> Tweet</a>
     </div>
 );
