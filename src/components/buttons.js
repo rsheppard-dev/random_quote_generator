d9fee5ba495876fcd870c634c84a3968961404b9
rsheppard83 <rsheppard83@gmail.com>
2021-01-03 10:21:24 +0000

    button spacing

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,7 +1,7 @@
 const Buttons = ({handleclick, quote, author}) => (
     <div className="buttons pt-4 d-flex justify-content-around">
-        <button className="btn btn-dark" onClick={handleclick}><i className="fas fa-comment-dots"></i> New Quote</button>
-        <a href={`http://www.twitter.com/intent/tweet?text=${quote} --${author}`} target="_blank" rel="noreferrer" className="btn btn-dark"><i className="fab fa-twitter-square"></i> Tweet</a>
+        <button className="btn btn-dark mr-1" onClick={handleclick}><i className="fas fa-comment-dots"></i> New Quote</button>
+        <a href={`http://www.twitter.com/intent/tweet?text=${quote} --${author}`} target="_blank" rel="noreferrer" className="btn btn-dark ml-1"><i className="fab fa-twitter-square"></i> Tweet</a>
     </div>
 );
 
