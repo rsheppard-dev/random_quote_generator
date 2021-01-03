727e030b4af646956019b3c3e3c90b8a69b06449
rsheppard83 <rsheppard83@gmail.com>
2021-01-03 10:24:18 +0000

    user stories

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,7 +1,7 @@
 const Buttons = ({handleclick, quote, author}) => (
     <div className="buttons pt-4 d-flex justify-content-around">
-        <button className="btn btn-dark mr-1" onClick={handleclick}><i className="fas fa-comment-dots"></i> New</button>
-        <a href={`http://www.twitter.com/intent/tweet?text=${quote} --${author}`} target="_blank" rel="noreferrer" className="btn btn-dark ml-1"><i className="fab fa-twitter-square"></i> Tweet</a>
+        <button id="new-quote" className="btn btn-dark mr-1" onClick={handleclick}><i className="fas fa-comment-dots"></i> New</button>
+        <a href={`http://www.twitter.com/intent/tweet?text=${quote} --${author}`} target="_blank" rel="noreferrer" className="btn btn-dark ml-1" id="tweet-quote"><i className="fab fa-twitter-square"></i> Tweet</a>
     </div>
 );
 
